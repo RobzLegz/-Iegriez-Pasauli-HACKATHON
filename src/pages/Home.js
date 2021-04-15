afba5a09acb5f3a58d1ea405fa8cff5e6e295856
Robzlegz <robzlegz@gmail.com>
2021-04-15 14:58:06 +0300

    initial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Home() {
+    return (
+        <HomePage>
+            <h1>Home</h1>
+        </HomePage>
+    )
+}
+const HomePage = styled.div`
+
+`;
+
+export default Home
