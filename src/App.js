ecc5a68162387a8bf77ab25dd87895791675220f
Robzlegz <robzlegz@gmail.com>
2021-04-16 09:59:02 +0300

    Co-authored-by: realman.23@inbox.lv <realman.23@inbox.lv>

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ function App() {
   const wheelRef = useRef();
 
   //Ģenerē nejaušu grādu skaitli (par cik grādiem pagriezīsies ritenis)
-  let randDegrees = Math.random() * 5000;
+  let wheelStops = [
+    {deg: 685, value: "banana"},
+    {deg: 725, value: "shirt"},
+    {deg: 1550, value: "longboard"},
+    {deg: 1620, value: "headphones"},
+    {deg: 1700, value: "burger"}
+  ]
 
+  let randomStop = wheelStops[Math.floor(Math.random() * 4) + 0]
+
+  //Iegriež ratu
   const SpinTheWheel = () => {
-    setSpinAgain(false);
+    setSpinAgain(false); //Ja rats griežas, neļaut iegriezt vēlreiz
     setSpinCounter(spinCounter + 1);
-    wheelRef.current.style.transform = `rotate(${randDegrees}deg)`;
+    wheelRef.current.style.transform = `rotate(${randomStop.deg}deg)`;
     setTimeout(() => {
-      setSpinAgain(true);
+      setSpinAgain(true);//Kad rats beidz griezties, atļaut iegriezt velreiz
+      console.log(randomStop.value, randomStop.deg)
     }, 5000);
   };
 
