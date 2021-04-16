da18c616bd662ac11180c0c664cb6f6a03a392b8
Robzlegz <robzlegz@gmail.com>
2021-04-16 17:38:29 +0300

    styling buttons

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
     body{
-        ::-webkit-scrollbar {
+        /* ::-webkit-scrollbar {
             width: 10px;
         }
         ::-webkit-scrollbar-track {
@@ -15,17 +15,31 @@ const GlobalStyles = createGlobalStyle`
             :hover {
                 background: #3c3c58;
             }
-        }
+        } */
         overflow: hidden;
     }
     button, 
     input{
         border-radius: 5px;
+        outline: none;
     }
     button{
         cursor: pointer;
         margin-top: 20px;
-        background: #ffffff !important;
+        background: #e6e6e6 !important;
+        border: 1px solid #3c3c58 !important;
+        color: #3c3c58;
+        font-size: 1rem;
+        padding: 5px 10px !important;
+        transition: background 0.3s ease;
+        :disabled{
+            :hover{
+                background: #e6e6e6 !important;
+            }
+        }
+        :hover{
+            background: #f3f2f2 !important;
+        }
     }
 `;
 
