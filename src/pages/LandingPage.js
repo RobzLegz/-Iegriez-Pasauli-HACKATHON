d43844d073358eb78e9f561797bc8ece4de44136
Robzlegz <robzlegz@gmail.com>
2021-04-18 23:21:00 +0300

    tidying up the code

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,7 +11,7 @@ function LandingPage({instructionState, setInstructionState}) {
         <StyledLandingPage>
             <h1>Iegriez Pasauli</h1>
             <div className="earth"></div>
-                <div className="pogas">
+            <div className="pogas">
                 <button onClick={() => history.push("/game")}><span>Sākt spēli</span>
                     <div className="liquid"></div>
                 </button>
@@ -30,7 +30,7 @@ function LandingPage({instructionState, setInstructionState}) {
     )
 }
 const StyledLandingPage = styled.div`
-        padding: 100px;
+    padding: 100px;
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -106,42 +106,40 @@ const StyledLandingPage = styled.div`
     }
     button .liquid::after,
     button .liquid::before {
-    content: '';
-    width: 175%;
-    height: 175%;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translate(-50%, -75%);
+        content: '';
+        width: 175%;
+        height: 175%;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translate(-50%, -75%);
     }
     button .liquid::before {
-  
         border-radius: 45%;
         background: rgba(255, 0, 0, .1);
         animation: animate 5s linear infinite;
-      }
+    }
       
-      button .liquid::after {
-        
+    button .liquid::after {
         border-radius: 40%;
         background: rgba(20, 20, 20, .5);
         animation: animate 10s linear infinite;
-      }
+    }
       
-      button:hover .liquid{
+    button:hover .liquid{
         top: -120px;
-      }
+    }
       
-      @keyframes animate {
+    @keyframes animate {
         0% {
-          transform: translate(-50%, -75%) rotate(0deg);
+            transform: translate(-50%, -75%) rotate(0deg);
         }
         100% {
-          transform: translate(-50%, -75%) rotate(360deg);
+            transform: translate(-50%, -75%) rotate(360deg);
         }
-      }
+    }
 
-      button .liquid1{
+    button .liquid1{
         position: absolute;
         top: -80px;
         left: 0;
@@ -154,41 +152,39 @@ const StyledLandingPage = styled.div`
     
     button .liquid1::after,
     button .liquid1::before {
-    content: '';
-    width: 175%;
-    height: 175%;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translate(-50%, -75%);
-    background: rgb(255, 255, 255);
+        content: '';
+        width: 175%;
+        height: 175%;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translate(-50%, -75%);
+        background: rgb(255, 255, 255);
     }
     button .liquid1::before {
-  
         border-radius: 45%;
         background: rgba(255, 0, 0, .1);
         animation: animate1 5s linear infinite;
-      }
+    }
       
-      button .liquid1::after {
-        
+    button .liquid1::after {
         border-radius: 40%;
         background: rgba(20, 20, 20, .5);
         animation: animate1 10s linear infinite;
-      }
+    }
       
-      button:hover .liquid1{
+    button:hover .liquid1{
         top: -120px;
-      }
+    }
       
-      @keyframes animate1 {
+    @keyframes animate1 {
         0% {
-          transform: translate(-50%, -75%) rotate(0deg);
+            transform: translate(-50%, -75%) rotate(0deg);
         }
         100% {
-          transform: translate(-50%, -75%) rotate(360deg);
+            transform: translate(-50%, -75%) rotate(360deg);
         }
-      }  
+    }  
     h1{
         color:#9aca3c;
         font-size:100px;
@@ -229,15 +225,14 @@ const StyledLandingPage = styled.div`
     }
 
     @keyframes spin{
-    from{
-        background-position-x: 0;
-    }
-    to{
-        background-position-x: 1100px;
-    }
+        from{
+            background-position-x: 0;
+        }
+        to{
+            background-position-x: 1100px;
+        }
     }
     .earth:hover{
-       
         -webkit-transform: scale(1.1) rotate(7deg);
         transform: scale(1.1) rotate(7deg);
     }
@@ -254,200 +249,197 @@ const StyledLandingPage = styled.div`
         }
         #instrukcija{
             margin-top:50px;
-        }
-       
-        
+        }        
     }
     @media (max-width:1175px){
-      .earth{
-        margin-top:-50px;
-        height:500px;
-        width:500px;
-        background-size:900px;
-        animation-name: spin;
-        animation-duration:12s;
-        -webkit-animation-duration: 12s;
-        @keyframes spin{
-          from{
-              background-position-x: 0;
-          }
-          to{
-              background-position-x: 900px;
-          }
-          }
-      }
-      h1{
-        font-size:80px;
-        margin-top:150px;
-      }
-
+        .earth{
+            margin-top:-50px;
+            height:500px;
+            width:500px;
+            background-size:900px;
+            animation-name: spin;
+            animation-duration:12s;
+            -webkit-animation-duration: 12s;
+            @keyframes spin{
+                from{
+                    background-position-x: 0;
+                }
+                to{
+                    background-position-x: 900px;
+                }
+            }
+        }
+        h1{
+            font-size:80px;
+            margin-top:150px;
+        }
     }
   
     @media (max-width:903px){
-      .earth{
-        margin-top:-50px;
-        height:500px;
-        width:500px;
-        background-size:900px;
-        animation-name: spin;
-        animation-duration:10s;
-        -webkit-animation-duration: 10s;
-        @keyframes spin{
-          from{
-              background-position-x: 0;
-          }
-          to{
-              background-position-x: 900px;
-          }
-          }
-      }
-    h1{
-      font-size:70px;
-      margin-top:150px;
-      -webkit-text-stroke: 4px black;
-    }
+        .earth{
+            margin-top:-50px;
+            height:500px;
+            width:500px;
+            background-size:900px;
+            animation-name: spin;
+            animation-duration:10s;
+            -webkit-animation-duration: 10s;
+            @keyframes spin{
+                from{
+                    background-position-x: 0;
+                }
+                to{
+                    background-position-x: 900px;
+                }
+            }
+        }
+        h1{
+            font-size:70px;
+            margin-top: 150px;
+            -webkit-text-stroke: 4px black;
+        }
     }
 
     @media (max-width:565px){
-      .earth{
-          width:400px;
-          height:400px;
-          background-size:700px;
-          animation-name: spin;
-          animation-duration:12s;
-          -webkit-animation-duration: 12s;
+        .earth{
+            width:400px;
+            height:400px;
+            background-size:700px;
+            animation-name: spin;
+            animation-duration:12s;
+            -webkit-animation-duration: 12s;
 
-          @keyframes spin{
-            from{
-                background-position-x: 0;
-            }
-            to{
-                background-position-x: 700px;
-            }
+            @keyframes spin{
+                from{
+                    background-position-x: 0;
+                }
+                to{
+                    background-position-x: 700px;
+                }
             }
         }
-      h1{
-        font-size:70px;
-      }
+        h1{
+            font-size:70px;
+        }
     }
     
-  @media (max-width:467px){
-    .earth{
-        width:350px;
-        height:350px;
-        background-size:650px;
-        animation-name: spin;
-        animation-duration:10s;
-        -webkit-animation-duration: 10s;
+    @media (max-width:467px){
+        .earth{
+            width:350px;
+            height:350px;
+            background-size:650px;
+            animation-name: spin;
+            animation-duration:10s;
+            -webkit-animation-duration: 10s;
 
-        @keyframes spin{
-            from{
-                background-position-x: 0;
-            }
-            to{
-                background-position-x:650px;
-            }
-        }
-      }
-
-    }
-  h1{
-    margin-top:150px;
-  } 
-}
-
-@media (max-width:393px){
-  .earth{
-      width:300px;
-      height:300px;
-      background-size:550px;
-      animation-name: spin;
-      animation-duration:12s;
-      -webkit-animation-duration: 12s;
-  }
-
-  @keyframes spin{
-    from{
-        background-position-x: 0;
-    }
-    to{
-        background-position-x:550px;
-    }
-  }
-  h1{
-    margin-top:150px;
-    font-size:60px;
-  } 
-}
-@media (max-width:321px){
-    .earth{
-        width:250px;
-        height:250px;
-        background-size:450px;
-        animation-name: spin;
-        animation-duration:12s;
-        -webkit-animation-duration: 12s;
-        @keyframes spin{
-            from{
-                background-position-x: 0;
-            }
-            to{
-                background-position-x: 450px;
+            @keyframes spin{
+                from{
+                    background-position-x: 0;
+                }
+                to{
+                    background-position-x:650px;
+                }
             }
         }
+
     }
     h1{
-      font-size:50px;
-      -webkit-text-stroke: 3px black;
-    }
-  .pogas{ 
-    margin-top:200px;
-  }
+        margin-top:40px;
+    } 
 
+
+    @media (max-width:393px){
+        .earth{
+            width:300px;
+            height:300px;
+            background-size:550px;
+            animation-name: spin;
+            animation-duration:12s;
+            -webkit-animation-duration: 12s;
+        }
+
+        @keyframes spin{
+            from{
+                background-position-x: 0;
+            }
+            to{
+                background-position-x:550px;
+            }
+        }
+        h1{
+            margin-top:10px;
+            font-size:60px;
+        } 
+    }
+    @media (max-width:321px){
+        .earth{
+            width:250px;
+            height:250px;
+            background-size:450px;
+            animation-name: spin;
+            animation-duration:12s;
+            -webkit-animation-duration: 12s;
+            @keyframes spin{
+                from{
+                    background-position-x: 0;
+                }
+                to{
+                    background-position-x: 450px;
+                }
+            }
+        }
+        h1{
+            font-size:50px;
+            -webkit-text-stroke: 3px black;
+        }
+        .pogas{ 
+            margin-top:0px;
+        }
+    }
 `;
 
 const StyledIntsructions = styled.div`
-position: absolute;
-top: 0;
-left: 0;
-z-index: 100;
-color:black;
-width: 100%;
-min-height:100vh;
-height:fit-content;
-display: flex;
-flex-direction: column;
-align-items: center;
-background-color:white;
-border-radius:10px;
-animation:instructions 0.75s;
-overflow:hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    color:black;
+    width: 100%;
+    min-height:100vh;
+    height:fit-content;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color:white;
+    border-radius:10px;
+    animation:instructions 0.75s;
+    overflow:hidden;
 
 
-.close{
-    height:37px;
-    width:37px;
-    position:absolute;
-    top:25px;
-    right:25px;
-    cursor:pointer;
-}
-
-@keyframes instructions {
-    from {
-    opacity: 0;
-    transform: translateX(10px);
+    .close{
+        height:37px;
+        width:37px;
+        position:absolute;
+        top:25px;
+        right:25px;
+        cursor:pointer;
     }
-    to {
-    opacity: 1;
-    transform: translateX(0);
+
+    @keyframes instructions {
+        from {
+        opacity: 0;
+        transform: translateX(10px);
+        }
+        to {
+        opacity: 1;
+        transform: translateX(0);
+        }
     }
-}
-h1{
-  align-items: center;
-  margin-top:auto;
-  margin-bottom:auto;
-}
+    h1{
+    align-items: center;
+    margin-top:auto;
+    margin-bottom:auto;
+    }
 `;
 
 export default LandingPage
