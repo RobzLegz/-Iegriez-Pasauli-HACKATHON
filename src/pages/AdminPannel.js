c0ab20eb5e14ef4bf07a2f6dca64a78a4d931992
Robzlegz <robzlegz@gmail.com>
2021-04-23 21:43:32 +0300

    api update

--- src/pages/AdminPannel.js
+++ src/pages/AdminPannel.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function AdminPannel({adminQuestions}) {
     return (
-        <div>
-            <h1>admin</h1>
+        <StyledAdminPannel>
+            <h1>admin pannel</h1>
             {adminQuestions.map((adminQuestion) => (
-                <div key={adminQuestion.choice_text}>
+                <div className="qeustion__container" key={adminQuestion.choice_text}>
                     <p>{adminQuestion.question}</p>
                     <p>{adminQuestion.choice_text}</p>
                     <p>{adminQuestion.correct}</p>
                 </div>
             ))}
-        </div>
+        </StyledAdminPannel>
     )
 }
+const StyledAdminPannel = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    >h1{
+        margin-bottom: 20px;
+    }
+    >.qeustion__container{
+        width: 90%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        border-top: 1px solid lightgray;
+        height: 40px;
+    }
+`;
 
 export default AdminPannel
