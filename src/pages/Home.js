10338640ceacff7c744c98527d637cd2bdf2330e
Robzlegz <robzlegz@gmail.com>
2021-04-30 18:42:28 +0300

    Co-authored-by: realman.23@inbox.lv <realman.23@inbox.lv>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Spinner from "../spinner/Spinner";
 import FirstStageQuestions from "../firstStage/FirstStageQuestions";
